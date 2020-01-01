314c572a96df7fb8a93511120b19babd7d3cde6e
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-02 00:32:22 +0300

    Added a view model EmployeeView.

---
+++ ASP_NET_Core_MVC_Level_1/ViewModels/EmployeeView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Core_MVC_Level_1.ViewModels
+{
+    public class EmployeeView
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SecondName { get; set; }
+    }
+}
