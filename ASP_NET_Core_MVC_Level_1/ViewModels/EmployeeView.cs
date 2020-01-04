b2b00c0a29fbe6e00d4e81770f822e04fd11bb11
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-04 15:42:31 +0300

    Added attributes to display properties in the EmployeeView class.

--- ASP_NET_Core_MVC_Level_1/ViewModels/EmployeeView.cs
+++ ASP_NET_Core_MVC_Level_1/ViewModels/EmployeeView.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_Core_MVC_Level_1.ViewModels
 {
     public class EmployeeView
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
 
+        [Display(Name = "Имя")]
         public string Name { get; set; }
 
+        [Display(Name = "Фамилия", Description = "Фамилия у этого сотрудника")]
         public string SecondName { get; set; }
     }
 }
