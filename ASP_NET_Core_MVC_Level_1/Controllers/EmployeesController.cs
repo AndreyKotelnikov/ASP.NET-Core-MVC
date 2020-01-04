d43885f68a36664eeba1ec40e38512ebc73a9f24
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-04 14:15:55 +0300

    Created a separate controller for employees and moved the view to a separate folder.

---
+++ ASP_NET_Core_MVC_Level_1/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_NET_Core_MVC_Level_1.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Core_MVC_Level_1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IEnumerable<EmployeeView> _employeeViews = Enumerable.Range(1, 10)
+        .Select(e => new EmployeeView
+        {
+            Id = e,
+            Name = $"Name {e}",
+            SecondName = $"SecondName {e}"
+        });
+
+        public IActionResult Index() => View(_employeeViews);
+    }
+}
--- ASP_NET_Core_MVC_Level_1/Controllers/HomeController.cs
+++ ASP_NET_Core_MVC_Level_1/Controllers/HomeController.cs
@@ -10,37 +10,24 @@ namespace ASP_NET_Core_MVC_Level_1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IConfiguration _configuration;
+        public IActionResult Index() => View();
 
-        public HomeController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+        public IActionResult Blog() => View();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult BlogSingle() => View();
 
-        public IActionResult ReadConfig()
-        {
-            return Content(_configuration["CustomData"]);
-        }
+        public IActionResult Cart() => View();
 
-        public IActionResult GetEmployees()
-        {
-            ViewBag.SomeData = "Привет от ViewBag";
-            ViewData["test"] = "Hello from ViewData";
-            
-            var emplist = Enumerable.Range(1, 10)
-                .Select(e => new EmployeeView
-            {
-                Id = e, 
-                Name = $"Name {e}", 
-                SecondName = $"SecondName {e}"
-            });
+        public IActionResult Checkout() => View();
 
-            return View(emplist);
-        }
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
+
+        public IActionResult Error404() => View();
     }
 }
