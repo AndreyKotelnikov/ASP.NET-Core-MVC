46864a12352e38cc0743dcf31d08127647eebe06
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-04 15:40:03 +0300

    Add details view for employee and some actions for work with this view.

--- ASP_NET_Core_MVC_Level_1/Controllers/EmployeesController.cs
+++ ASP_NET_Core_MVC_Level_1/Controllers/EmployeesController.cs
@@ -18,5 +18,54 @@ namespace ASP_NET_Core_MVC_Level_1.Controllers
         });
 
         public IActionResult Index() => View(_employeeViews);
+
+        public IActionResult Details(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest();
+            }
+
+            var employee = _employeeViews.FirstOrDefault(e => e.Id == id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        public IActionResult DetailsSearchByNames(string name, string secondName)
+        {
+            if (name is null && secondName is null)
+            {
+                return BadRequest();
+            }
+
+            var sellectEmployees = _employeeViews;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                sellectEmployees = sellectEmployees.Where(e => e.Name == name);
+            }
+            if (!string.IsNullOrWhiteSpace(secondName))
+            {
+                sellectEmployees = sellectEmployees.Where(e => e.SecondName == secondName);
+            }
+
+            var employee = sellectEmployees.FirstOrDefault();
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            return View(nameof(Details), employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [FromBody] EmployeeView employee)
+        {
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
