bad645a3f80c258b1400c4d1f820eaa60824f003
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-05 14:46:38 +0300

    Created the examples of responses in TestAction that can be returned by the action controller.

--- ASP_NET_Core_MVC_Level_1/Controllers/HomeController.cs
+++ ASP_NET_Core_MVC_Level_1/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace ASP_NET_Core_MVC.Controllers
 {
@@ -23,5 +27,32 @@ namespace ASP_NET_Core_MVC.Controllers
         public IActionResult Shop() => View();
 
         public IActionResult Error404() => View();
+
+        public IActionResult TestAction()
+        {
+            //return View();
+            //return new ViewResult();
+
+            //return Json(new {Name = "Andrey", Id = 1}, new JsonSerializerOptions(){ MaxDepth = 1});
+            //return new JsonResult(new {Name = "Andrey", Id = 1});
+
+            //return Redirect("http://www.yandex.ru");
+            //return new RedirectResult("http://www.yandex.ru");
+
+            return RedirectToAction("Index", "Home");
+            //return new RedirectToActionResult("Index", "Home", null);
+
+            //return Content("Hello!", "application/text", Encoding.UTF8);
+            //return new ContentResult{ Content = "Hello!", StatusCode = 200, ContentType = "application/text"};
+
+            //return File(Encoding.UTF8.GetBytes("Hello world!"), "application/text", "HelloWorld.txt");
+            //return new FileContentResult(Encoding.UTF8.GetBytes("Hello world!"), new MediaTypeHeaderValue("application/text", 5));
+            //return new FileStreamResult(new MemoryStream(Encoding.UTF8.GetBytes("Hello world!")), "application/text");
+
+            //return StatusCode(200);
+            //return new StatusCodeResult(200);
+            //return NoContent();
+            //return new NoContentResult();
+        }
     }
 }
