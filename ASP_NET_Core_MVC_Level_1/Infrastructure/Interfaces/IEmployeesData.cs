b981acc46810d01ed4f2151223d646c8c60cf9db
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-05 17:11:36 +0300

    Created an interface IEmployeesData for working with employees with CRUD operations.

---
+++ ASP_NET_Core_MVC_Level_1/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_NET_Core_MVC.ViewModels;
+
+namespace ASP_NET_Core_MVC.Infrastructure.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<EmployeeView> GetAll();
+
+        EmployeeView GetById(int id);
+
+        void Add(EmployeeView employee);
+
+        void Edit(int id, EmployeeView employee);
+
+        bool Delete(int id);
+
+        bool SaveChanges();
+    }
+}
