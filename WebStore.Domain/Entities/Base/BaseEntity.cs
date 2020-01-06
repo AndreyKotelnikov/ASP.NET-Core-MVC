89290eb202b45856646546338d6227461c785565
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-06 18:46:14 +0300

    Created basic abstract entities.

---
+++ WebStore.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities.Base
+{
+    /// <summary>
+    /// Сущность
+    /// </summary>
+    public abstract class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
