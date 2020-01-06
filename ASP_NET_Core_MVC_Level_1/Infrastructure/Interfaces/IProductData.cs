822ffe0890dc64bc267ce26afc7d860a3a18772f
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-06 19:28:56 +0300

    Implemented service IProductData.

---
+++ ASP_NET_Core_MVC_Level_1/Infrastructure/Interfaces/IProductData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+
+namespace ASP_NET_Core_MVC.Infrastructure.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Brand> GetBrands();
+
+        IEnumerable<Section> GetSections();
+    }
+}
---
+++ ASP_NET_Core_MVC_Level_1/Infrastructure/Services/InMemoryProductData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_NET_Core_MVC.Data;
+using ASP_NET_Core_MVC.Infrastructure.Interfaces;
+using WebStore.Domain.Entities;
+
+namespace ASP_NET_Core_MVC.Infrastructure.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Brand> GetBrands() => TestData.Brands;
+       
+        public IEnumerable<Section> GetSections() => TestData.Sections;
+    }
+}
