5ff269bedb7043258e3771107501273b68c7525c
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-06 22:44:25 +0300

    Create ViewComponents for Brands and Sections.

---
+++ ASP_NET_Core_MVC_Level_1/Components/BrandsViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_NET_Core_MVC.Infrastructure.Interfaces;
+using ASP_NET_Core_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Core_MVC.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductData _productData;
+
+        public BrandsViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        public IViewComponentResult Invoke() => View(GetBrandViewModels());
+
+        private IEnumerable<BrandViewModel> GetBrandViewModels()
+        {
+            var brandViewModels = _productData.GetBrands()
+                .Select(b => new BrandViewModel
+                    {
+                        Id = b.Id,
+                        Name = b.Name,
+                        Order = b.Order
+                    })
+                .OrderBy(b => b.Order)
+                .ToList();
+
+            return brandViewModels;
+        }
+    }
+}
---
+++ ASP_NET_Core_MVC_Level_1/Components/SectionsViewComponent.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_NET_Core_MVC.Infrastructure.Interfaces;
+using ASP_NET_Core_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Core_MVC.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        private readonly IProductData _productData;
+
+        public SectionsViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        public IViewComponentResult Invoke() => View(GetSectionViewModels());
+
+        private IEnumerable<SectionViewModel> GetSectionViewModels()
+        {
+            var sectionsWithoutParent = _productData.GetSections()
+                .Where(s => s.ParentId is null)
+                .ToArray();
+
+            var sectionViewModels = sectionsWithoutParent.Select(s =>
+                new SectionViewModel
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Order = s.Order,
+                    ParentSection = null
+                })
+                .ToList();
+
+            foreach (var sectionViewModel in sectionViewModels)
+            {
+                var childSections = _productData.GetSections()
+                    .Where(s => s.ParentId == sectionViewModel.Id)
+                    .ToArray();
+
+                foreach (var childSection in childSections)
+                {
+                    sectionViewModel.ChildSections.Add(new SectionViewModel
+                    {
+                        Id = childSection.Id,
+                        Name = childSection.Name,
+                        Order = childSection.Order,
+                        ParentSection = sectionViewModel
+                    });
+                }
+                sectionViewModel.ChildSections.Sort((x, y) => Comparer<int>.Default.Compare(x.Order, y.Order));
+            }
+            
+            sectionViewModels.Sort((x, y) => Comparer<int>.Default.Compare(x.Order, y.Order));
+
+            return sectionViewModels;
+        }
+    }
+}
