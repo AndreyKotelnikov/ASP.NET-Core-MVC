e27cba1b72456364dda93c1fa2f61ad427329493
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-07 01:12:45 +0300

    Create controller Catalog.

---
+++ ASP_NET_Core_MVC_Level_1/Controllers/CatalogController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_NET_Core_MVC.Infrastructure.Interfaces;
+using ASP_NET_Core_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+
+namespace ASP_NET_Core_MVC.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _productData;
+
+        public CatalogController(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        public IActionResult Shop(int? brandId, int? sectionId)
+        {
+            var products = _productData.GetProducts(new ProductFilter
+            {
+                BrandId = brandId,
+                SectionId = sectionId
+            });
+
+            var catalog = new CatalogViewModel
+            {
+                BrandId = brandId,
+                SectionId = sectionId,
+                Products = products.Select(p =>
+                        new ProductViewModel
+                        {
+                            Id = p.Id,
+                            Name = p.Name,
+                            Order = p.Order,
+                            ImageUrl = p.ImageUrl,
+                            Price = p.Price
+                        })
+                    .OrderBy(p => p.Order)
+            };
+            
+            return View(catalog);
+        }
+
+        public IActionResult Detailes()
+        {
+            return View();
+        }
+    }
+}
