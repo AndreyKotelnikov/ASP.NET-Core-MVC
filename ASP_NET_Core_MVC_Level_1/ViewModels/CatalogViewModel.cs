dbfd5d587f9a7a1e49dc9f531b91a391a9b12530
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-07 01:11:44 +0300

    Create view models: Product and Catalog.

---
+++ ASP_NET_Core_MVC_Level_1/ViewModels/CatalogViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ASP_NET_Core_MVC.ViewModels
+{
+    public class CatalogViewModel
+    {
+        public int? BrandId { get; set; }
+
+        public int? SectionId { get; set; }
+
+        public IEnumerable<ProductViewModel> Products { get; set; }
+    }
+}
