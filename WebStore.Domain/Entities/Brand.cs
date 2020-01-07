0b4f909a4b69258f002d87c82c606539dd8b8de7
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-07 17:25:32 +0300

    Added restrictions to entities for working with databases via annotations. And added navigation properties.

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -9,5 +9,7 @@ namespace WebStore.Domain.Entities
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }  
     }
 }
--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
@@ -12,10 +13,17 @@ namespace WebStore.Domain.Entities
 
         public int SectionId { get; set; }
 
+        [ForeignKey(nameof(SectionId))]
+        public virtual Section Section { get; set; }
+
         public int? BrandId { get; set; }
+
+        [ForeignKey(nameof(BrandId))]
+        public virtual Brand Brand { get; set; }
 
         public string ImageUrl { get; set; }
 
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
