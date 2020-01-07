5503267730e86c81158f4596513279f3621f4a89
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-07 17:50:54 +0300

    Create DbContext.

---
+++ WebStore.DAL/Context/WebStoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreDbContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+        
+        public WebStoreDbContext(DbContextOptions<WebStoreDbContext> options) : base(options) { }
+    }
+}
