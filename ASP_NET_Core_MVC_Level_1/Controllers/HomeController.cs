e5ddc1167d2bb64c5d74b6a3d9018129641d3072
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-07 20:31:57 +0300

    Removed some of the test cases and comments that will not be used.

--- ASP_NET_Core_MVC_Level_1/Controllers/HomeController.cs
+++ ASP_NET_Core_MVC_Level_1/Controllers/HomeController.cs
@@ -25,32 +25,5 @@ namespace ASP_NET_Core_MVC.Controllers
         public IActionResult ProductDetails() => View();
 
         public IActionResult Error404() => View();
-
-        public IActionResult TestAction()
-        {
-            //return View();
-            //return new ViewResult();
-
-            //return Json(new {Name = "Andrey", Id = 1}, new JsonSerializerOptions(){ MaxDepth = 1});
-            //return new JsonResult(new {Name = "Andrey", Id = 1});
-
-            //return Redirect("http://www.yandex.ru");
-            //return new RedirectResult("http://www.yandex.ru");
-
-            return RedirectToAction("Index", "Home");
-            //return new RedirectToActionResult("Index", "Home", null);
-
-            //return Content("Hello!", "application/text", Encoding.UTF8);
-            //return new ContentResult{ Content = "Hello!", StatusCode = 200, ContentType = "application/text"};
-
-            //return File(Encoding.UTF8.GetBytes("Hello world!"), "application/text", "HelloWorld.txt");
-            //return new FileContentResult(Encoding.UTF8.GetBytes("Hello world!"), new MediaTypeHeaderValue("application/text", 5));
-            //return new FileStreamResult(new MemoryStream(Encoding.UTF8.GetBytes("Hello world!")), "application/text");
-
-            //return StatusCode(200);
-            //return new StatusCodeResult(200);
-            //return NoContent();
-            //return new NoContentResult();
-        }
     }
 }
