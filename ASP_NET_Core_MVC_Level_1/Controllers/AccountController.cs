1420593581f32f65ceba7aa95da5935c15b46e24
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-11 16:14:27 +0300

    Implementation of the logging system in the account controller.

--- ASP_NET_Core_MVC_Level_1/Controllers/AccountController.cs
+++ ASP_NET_Core_MVC_Level_1/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ASP_NET_Core_MVC.ViewModels.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WebStore.Domain.Entities.Identity;
 
 namespace ASP_NET_Core_MVC.Controllers
@@ -13,11 +14,13 @@ namespace ASP_NET_Core_MVC.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly ILogger _logger;
 
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<AccountController> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _logger = logger;
         }
 
         public IActionResult Register() => View(new RegisterUserViewModel());
@@ -34,11 +37,14 @@ namespace ASP_NET_Core_MVC.Controllers
             {
                 UserName = modelUser.UserName
             };
+            _logger.LogInformation("Регистрация нового пользователя {0}", user.UserName);
 
             var registrationResult = await _userManager.CreateAsync(user, modelUser.Password);
             if (registrationResult.Succeeded)
             {
+                _logger.LogInformation("Пользователь {0} успешно зарегистрирован", user.UserName);
                 await _signInManager.SignInAsync(user, false);
+                _logger.LogInformation("Пользователь {0} вошёл в систему", user.UserName);
                 return RedirectToAction("Index", "Home");
             }
            
@@ -46,6 +52,8 @@ namespace ASP_NET_Core_MVC.Controllers
             {
                 ModelState.AddModelError("", identityError.Description);
             }
+            _logger.LogInformation("Ошибка при регистрации пользователя {0}: {1}", user.UserName, 
+                string.Join(", ", registrationResult.Errors.Select(err => err.Description)));
 
             return View(modelUser);
         }
@@ -72,6 +80,7 @@ namespace ASP_NET_Core_MVC.Controllers
 
             if (loginResult.Succeeded)
             {
+                _logger.LogInformation("Пользователь {0} вошёл в систему", loginModel.UserName);
                 if (Url.IsLocalUrl(loginModel.ReturnUrl))
                 {
                     return Redirect(loginModel.ReturnUrl);
@@ -81,6 +90,7 @@ namespace ASP_NET_Core_MVC.Controllers
             }
 
             ModelState.AddModelError("", "Указаны некорректные имя или пароль");
+            _logger.LogWarning("Ошибка при входе пользователя {0} в систему", loginModel.UserName);
             return View(loginModel);
         }
 
@@ -88,6 +98,7 @@ namespace ASP_NET_Core_MVC.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
+            _logger.LogInformation("Пользователь {0} вышел из системы", User.Identity.Name);
             return RedirectToAction("Index", "Home");
         }
     }
