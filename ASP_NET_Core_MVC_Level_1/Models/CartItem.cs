961e1788df28e7fb0e42c55f881bca31c65fdb15
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-15 20:47:57 +0300

    Create models: Cart and CartItem.

---
+++ ASP_NET_Core_MVC_Level_1/Models/CartItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Core_MVC.Models
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
