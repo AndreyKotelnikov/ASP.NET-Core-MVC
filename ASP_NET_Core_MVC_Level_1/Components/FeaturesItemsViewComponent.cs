0b78ce7069ab7b78dd9e776cd37189b111cd09fb
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2020-01-16 19:56:48 +0300

    Create FeaturesItemsViewComponent.

---
+++ ASP_NET_Core_MVC_Level_1/Components/FeaturesItemsViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_NET_Core_MVC.Infrastructure.Interfaces;
+using ASP_NET_Core_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Core_MVC.Components
+{
+    public class FeaturesItemsViewComponent : ViewComponent
+    {
+        private readonly IProductData _productData;
+
+        public FeaturesItemsViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        public IViewComponentResult Invoke() => View(GetProductViewModels());
+
+        private IEnumerable<ProductViewModel> GetProductViewModels()
+        {
+            var productViewModels = _productData.GetProducts()
+                .Take(6)
+                .Select(p => new ProductViewModel
+            {
+                Id = p.Id,
+                Name = p.Name,
+                ImageUrl = p.ImageUrl,
+                Brand = p.Brand?.Name,
+                Order = p.Order,
+                Price = p.Price
+            }).OrderBy(p => p.Order)
+                .ToList();
+
+            return productViewModels;
+        }
+    }
+}
